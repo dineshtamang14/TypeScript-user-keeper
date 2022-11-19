d5839fe0ac5f686be0e0c9047457945ef6f20c38
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-11-19 11:48:41 +0530

    added useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
 
-  const firstName : string = "Dinesh Tamang";
-  console.log(firstName);
+  interface UserInterface {
+    name: string,
+    age: string,
+    job: string
+  }
+
+  const [userState, setUsersState] = useState<{ currentUser: UserInterface }>({
+    currentUser: { 
+      name: "",
+      age: "",
+      job: ""
+    }
+  })
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    
+  }
 
   return (
     <div className="container">
       <h1>TypeScript</h1>
       <form>
         <label htmlFor="userName">Name:</label>
-        <input type="text" name="name" id="userName" value="" />
+        <input type="text" name="name" id="userName" value=""
+          onChange={onChangeHandler}
+        />
 
         <label htmlFor="userEmail">Email:</label>
-        <input type="email" name="email" id="userEmail" value="" />
+        <input type="email" name="email" id="userEmail" value="" onChange={onChangeHandler} />
 
         <label htmlFor="userAge">Age:</label>
-        <input type="number" name="age" id="userAge" value="" />
+        <input type="number" name="age" id="userAge" value="" onChange={onChangeHandler} />
 
         <label htmlFor="userJob">Job:</label>
-        <input type="text" name="job" id="userJob" value="" />
+        <input type="text" name="job" id="userJob" value="" onChange={onChangeHandler} />
 
         <button type="submit">Add user</button>
       </form>
