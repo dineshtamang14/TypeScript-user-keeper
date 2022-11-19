1dedf2aafb2f293d9057eba9a80a99b1314aaebf
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-11-19 11:26:03 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const firstName : string = "Dinesh Tamang";
+  console.log(firstName);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TypeScript</h1>
+      <form>
+        <label htmlFor="userName">Name:</label>
+        <input type="text" name="name" id="userName" value="" />
+
+        <label htmlFor="userEmail">Email:</label>
+        <input type="email" name="email" id="userEmail" value="" />
+
+        <label htmlFor="userAge">Age:</label>
+        <input type="number" name="age" id="userAge" value="" />
+
+        <label htmlFor="userJob">Job:</label>
+        <input type="text" name="job" id="userJob" value="" />
+
+        <button type="submit">Add user</button>
+      </form>
     </div>
   );
 }
